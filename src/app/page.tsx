035e3c3c7216d9d83e5146e1a82c8b3842c98b85
Akhil Alex <akhilalex003z@gmail.com>
2024-05-22 00:04:15 +0530

    table creation in schema
    made users, workdiaries and session booking tables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
-import { db } from "@/server/db/index";
-import { users } from "@/server/db/schema";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div>
       <Link href="/sign-in">Sign In</Link>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,7 +2,32 @@ import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable('users', {
-  id: text('id').primaryKey(),
-  textModifiers: text('text_modifiers').notNull().default(sql`CURRENT_TIMESTAMP`),
-  intModifiers: integer('int_modifiers', { mode: 'boolean' }).notNull().default(false),
+  registerNum: integer('register_num').primaryKey(),
+  firstName: text('first_name'),
+  lastName: text('last_name'),
+  email: text('email'),
+  designation: text('designation'),
 });
+
+export const workdiaries = sqliteTable('workdiaries', {
+  entryID: integer('entry_id').primaryKey({autoIncrement: true}),
+  serialNum: integer('serial_num'),
+  workTitle: text('work_title'),
+  workDesc: text('work_description'),
+  startTime: text('start_time'),
+  endTime: text('end_time'),
+  hoursWorked: text('hours_worked'),
+  registerNum: integer('register_num').references(() => users.registerNum),
+})
+
+export const sessionBooking = sqliteTable('session_booking', {
+  sessionID: integer('session_id').primaryKey({autoIncrement: true}),
+  applicantRegID: integer('applicant_regID'),
+  applicantName: text('applicant_name'),
+  applicantDesig: text('applicant_designation'),
+  applicantEmail: text('applicant_email'),
+  applicantPhone: text('applicant_phone'),
+  sessionDate: text('session_date'),
+  sessionType: text('session_type').default("GPT"),
+  sessionTopic: text('session_topic'),
+})
