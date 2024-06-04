66a553b233ec09c4f6cb6f568f46562bd5a7dd0e
Akhil Alex <akhilalex003z@gmail.com>
2024-06-04 17:19:16 +0530

    removed clerk and middleware
    assigned auth work to vishnu, session booking to preetham

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default async function HomePage() {
   return (
     <div>
-      <Link href="/sign-in">Sign In</Link>
       <Link href="./session-booking">Book a GPT/OPT session</Link>
     </div>
   );
